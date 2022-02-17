b3f88b8686e271f91e1485f8d34b232269182bc7
AndinaZS <alexa.markova@outlook.com>
2022-02-17 23:33:28 +0300

    initial

---
+++ HW_gen.py
@@ -0,0 +1,20 @@
+def flat_generator(obj_list):
+    flag = True
+    while flag:
+        flag = False
+        for num, el in enumerate(obj_list):
+            if not isinstance(el, list):
+                yield el
+            else:
+                obj_list = el + obj_list[num + 1:]
+                flag = True
+                break
+        continue
+
+
+nested_list = [['a', 'b', 'c', [1, 2]],
+	['d', 'e', 'f', 'h', False, ['e', 'f', [1, 2], 'h'], ],
+	[1, 2, 3, [4]]
+]
+
+print(list(flat_generator(nested_list)))
---
+++ HW_iter.py
@@ -0,0 +1,41 @@
+class FlatIterator:
+
+	def __init__(self, obj_list):
+		self.obj_list = obj_list
+
+	def create_iterator(self, obj_list=None):
+		if not obj_list:
+			obj_list = self.obj_list
+		res_list = []
+		count = 0
+		for lst in obj_list:
+			if isinstance(lst, list):
+				count = 1
+				for el in lst:
+					res_list.append(el)
+			else:
+				res_list.append(lst)
+		if count:
+			return self.create_iterator(res_list)
+		return res_list
+
+	def __iter__(self):
+		self.iter_list = self.create_iterator()
+		self.cursor = -1
+		return self
+
+	def __next__(self):
+		self.cursor += 1
+		if self.cursor == len(self.iter_list):
+			raise StopIteration
+		return self.iter_list[self.cursor]
+
+
+nested_list = [
+	['a', 'b', 'c', [1, 2]],
+	['d', 'e', 'f', 'h', False, ['e', 'f', [1, 2], 'h'], ],
+	[1, 2]
+]
+
+flat_list = [item for item in FlatIterator(nested_list)]
+print(flat_list)
